797531d4a231f110d72b2a976d466f5e74757c4d
Steven Grant <blue.ninjaneer@gmail.com>
2019-08-09 13:45:58 -0700

    added logging messages

--- src/main/java/bitmap/BitMap.java
+++ src/main/java/bitmap/BitMap.java
@@ -11,6 +11,7 @@ public class BitMap {
     BufferedImage image;
 
     public BufferedImage getBmpImage(String targetFilePath)   {
+        System.out.println("Getting file");
     BufferedImage img = null;
     try {
         img = ImageIO.read(new File(targetFilePath));
@@ -21,13 +22,16 @@ public class BitMap {
     }
 
     public int[] getDementions(BufferedImage image){
+        System.out.println("Analizing file size.");
+
         int[]res = new int[2];
         res[0] = image.getHeight();
         res[1] = image.getWidth();
         return res;
     }
     public BufferedImage changeColorMultiplication(BufferedImage image, int height, int width, int mutator){
-    for(int y = 0; y< height; y++){
+
+        for(int y = 0; y< height; y++){
         for(int x = 0; x < width; x++){
             int pixel = image.getRGB(x,y);
             image.setRGB(x,y,pixel * mutator);
@@ -36,17 +40,26 @@ public class BitMap {
     return image;
     }
     public void saveImage(BufferedImage image, String path)  {
+        System.out.println("Saving new image.");
+
 //        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
 //        writer.write(image);
 
         try {
+            System.out.println("Saving new pic.");
+
             ImageIO.write(image,"bmp", new File(path));
         } catch (IOException e) {
+            System.out.println("Lol no.");
+
             e.printStackTrace();
         }
+        System.out.println("Done.");
+
 
     }
     public void invertColor(String inPath, String outPath){
+        System.out.println("Flip turning colors upside down.");
         BufferedImage img =getBmpImage(inPath);
         int[] dimentions = getDementions(img);
         img = changeColorMultiplication(img,dimentions[0],dimentions[1], -1);
